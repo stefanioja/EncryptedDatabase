0891a1956bd7346a263c04667f435048c53027ab
stefanioja <stefanvalentin.ioja@gmail.com>
2024-12-11 17:35:38 +0200

    minor bugfix and improved readability/performance

--- SCrypt/RSA.py
+++ SCrypt/RSA.py
@@ -1,4 +1,6 @@
-from SCrypt.NT import prime_gen, mod_inverse
+import random
+from math import gcd
+from SCrypt.NT import prime_gen
 
 def RSA_key_gen(key_len: int) -> tuple[tuple[int, int], tuple[int, int]]:
     """
@@ -13,14 +15,24 @@ def RSA_key_gen(key_len: int) -> tuple[tuple[int, int], tuple[int, int]]:
     p = prime_gen(key_len // 2)
     q = prime_gen(key_len // 2)
 
+    while p == q:
+        p = prime_gen(key_len // 2)
+        q = prime_gen(key_len // 2)
+    
     n = p * q
     phi = (p - 1) * (q - 1)
     if phi > 65537:
         e = 65537
+    elif phi > 17:
+        e = 17
     else:
         e = 3
 
-    d = mod_inverse(e, phi)
+    if phi % e == 0:
+        while gcd(e, phi) != 1:
+            e = random.randint(2, phi - 1)
+
+    d = pow(e, -1, phi)
 
     public_key = (e, n)
     private_key = (d, n)
