bfaf634af6f599eea72d5f37f028722080e8a18d
stefanioja <stefanvalentin.ioja@gmail.com>
2025-01-06 21:37:14 +0200

    NT.py updated to respect pep standard

--- SCrypt/NT.py
+++ SCrypt/NT.py
@@ -1,17 +1,21 @@
-""""""
+"""This module provides number theory functions.
+
+Functions:
+    prime_gen(size: int) -> int: Generate a random prime number.
+"""
+
 import random
 import sympy
 
 
 def prime_gen(size: int) -> int:
-    """
-    Generates a random prime number
+    """Generate a random prime number.
 
     Arguments:
-        size: size of the number to be generated (in bits)
+        size: size of the number to be generated (in bits).
 
     Returns:
-        A prime number that needs <=size bits to be written
+        A prime number that needs <=size bits to be written.
     """
     while True:
         p = random.randint(0, (1 << size) - 1)
